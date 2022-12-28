cb7c3f6f2105720b19d0d05ceecc1739f972897e
EthanG78 <ethangarnier78@gmail.com>
2022-12-28 00:29:21 -0400

    started work on mandelbrot exercise

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,17 @@
+use num::Complex;
+
 fn main() {
-    println!("Hello, world!");
+    let c: Complex<f64> = Complex { re: 1.0, im: -1.0 };
+}
+
+fn escape_time(c: Complex<f64>, iterations: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..iterations {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+
+    None
 }
